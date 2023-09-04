f1bb1153b169ac17ce398eb6e873da92771510d3
Tzipora Maroko <maroko@g.jct.ac.il>
2023-09-04 11:08:19 +0300

    sql data model

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -27,6 +27,11 @@ namespace WebApplication1.Controllers
             return View();
         }
 
+        public IActionResult Flavours()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
---
+++ WebApplication1/Models/Flavour.cs
@@ -0,0 +1,14 @@
+﻿using System.Drawing;
+
+namespace WebApplication1.Models
+{
+    public class Flavour
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public string ImageURL { get; set; }
+    }
+}
