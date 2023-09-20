aca9c8c14bb166436febd83fa41499d790876193
Tzipora Maroko <maroko@g.jct.ac.il>
2023-09-20 13:31:46 +0300

    i dont know

---
+++ ZeldaWebsite/Controllers/PayPalController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ZeldaWebsite.Controllers
+{
+	public class PayPalController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
